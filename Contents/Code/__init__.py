ec96c1bf479db4221b1ddf57cdf7ded57bc3700e
willjoha <github.com@brute-force.org>
2014-01-11 01:34:49 +0100

    Thumbnails added for the programs and videos - Fixes #3

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,6 @@
 ﻿import re
 from collections import OrderedDict
+from string import ascii_uppercase
 
 # +++++ ARD Mediathek Plugin for Plex +++++
 #
@@ -14,7 +15,9 @@ from collections import OrderedDict
 ####################################################################################################
 
 SENDUNGENAZURL = 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3551682/view=module/index.html'
+PROGRAMSAZURL = 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3474820/view=list/initial=%s/index.html'
 MOSTVIEWEDURL = 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3516192/view=list/show=%s/index.html'
+PROGRAMURL = 'http://www.ardmediathek.de/ard/servlet/ajax-cache/3516962/view=list/documentId=%s/goto=%s/index.html'
 
 RE_SENDUNGVERPASSTLISTE = Regex('var sendungVerpasstListe = (\[[^\]]+\]);')
 RE_DOCUMENTID = Regex('\?documentId=([0-9]+)$')
@@ -74,25 +77,24 @@ def MainMenu():
 def SendungenAZ(name):
     oc = ObjectContainer(title2=name, view_group="List")
     
+    oc.add(DirectoryObject(key=Callback(SendungenAZList, char='0-9'), title='0-9'))
     # A to Z
-    for page in SENDUNGENAZ:
-        oc.add(DirectoryObject(key=Callback(SendungenAZList, char=page), title=page))
+    for char in list(ascii_uppercase):
+        oc.add(DirectoryObject(key=Callback(SendungenAZList, char=char), title=char))
     
     return oc
 
 @route('/video/ardmediathek/sendungenaz/{char}')
 def SendungenAZList(char):
     oc = ObjectContainer(title2=char)
-    data = HTTP.Request(SENDUNGENAZURL, cacheTime=CACHE_1HOUR).content
-    list = RE_SENDUNGVERPASSTLISTE.findall(data, re.DOTALL)
-    jsondata = JSON.ObjectFromString(list[0], 'utf-8')
-    for sendung in jsondata:
-        if(SENDUNGENAZ[char].match(sendung['titel']) == None):
-            continue
-        Log(sendung)
-        title = html_decode(sendung['titel'])
-        documentId = RE_DOCUMENTID.findall(sendung['link'])[0]
-        oc.add(DirectoryObject(key=Callback(Sendung, name=title, documentId=documentId), title=title))
+    content = HTML.ElementFromURL(PROGRAMSAZURL % char).xpath('//div[@class="mt-media_item"]')
+    for item in content:
+        link  = item.xpath('./h3[@class="mt-title"]/a')[0]
+        title = link.text
+        documentId = RE_DOCUMENTID.findall(link.get('href'))[0]
+        summary = item.xpath('./p/span[@class="mt-count"]')[0].text + ' | ' + item.xpath('./p/span[@class="mt-channel"]')[0].text
+        thumb = BASE_URL + item.xpath('./div[@class="mt-image"]/img/@src')[0]
+        oc.add(DirectoryObject(key=Callback(Sendung, name = title, documentId=documentId), title = title, summary = summary, thumb = thumb))
     return oc
 
 @route('/video/ardmediathek/mostviewed/{type}')
@@ -114,25 +116,27 @@ def MostViewed(name, type):
         ))
     return oc
 
-
 @route('/video/ardmediathek/sendung/{name}/{documentId}')
-def Sendung(name, documentId):
+@route('/video/ardmediathek/sendung/{name}/{documentId}/{page}')
+def Sendung(name, documentId, page=1):
     oc = ObjectContainer(title2=name)
-
-    for item in XML.ElementFromURL('http://www.ardmediathek.de/export/rss/id=%s' % documentId).xpath('//item'):
-        url = item.xpath('./link/text()')[0]
-        title = item.xpath('./title/text()')[0]
-        summary = item.xpath('./description/text()')[0]
-        originally_available_at = Datetime.ParseDate(item.xpath('./pubDate/text()')[0])
-
+    content = HTML.ElementFromURL(PROGRAMURL % (documentId, page))
+    for item in content.xpath('//ol[@class="mt-view-level_3 mt-list_view"]/li/div[@class="mt-media_item"]'):
+        link  = item.xpath('./h3[@class="mt-title"]/a')[0]
+        title = link.text
+        url   = BASE_URL + link.get('href')
+        summary = item.xpath('./p[@class="mt-source mt-tile-view_hide"]')[0].text + ' | ' + item.xpath('./p[@class="mt-airtime_channel"]/span[@class="mt-channel mt-tile-view_hide"]')[0].text
+        thumb = BASE_URL + item.xpath('./div[@class="mt-image"]/img/@src')[0]
         oc.add(VideoClipObject(
             url = url,
             title = title,
             summary = summary,
-            originally_available_at = originally_available_at
+            thumb = thumb
         ))
-
+    if(len(content.xpath('//a[@class="mt-view-level-3_pager mt-icon mt-icon-arrowbig_right ajax-paging"]/@href')) == 1):
+        oc.add(NextPageObject(key=Callback(Sendung, name=name, documentId=documentId, page=str(int(page)+1)), title=str("Weitere Beiträge").decode('utf-8', 'strict'), summary=None, thumb="more.png"))
     return oc
+
 
 def html_decode(s):
     s = s.replace('&#039;', "'")
